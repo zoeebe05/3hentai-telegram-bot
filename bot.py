38b0aea600c7e74c647d7cdf2ab3509a1fe2577a
zoeebe05 <zoeebe05@gmail.com>
2024-08-21 12:54:05 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import re
 import requests
 from bs4 import BeautifulSoup
 from telebot import TeleBot
+import zipfile
 
 # Función para limpiar el nombre de la página
 def clean_filename(name):
@@ -60,6 +61,57 @@ def send_info(message):
         bot.send_photo(message.chat.id, photo, caption=clean_title)
 
     os.remove(image_path)
+    
+@bot.message_handler(commands=['d_codigo'])
+def handle_d_codigo(message):
+    url = 'https://www/d/código'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    
+    # Capturar el nombre de la página
+    page_title = soup.title.string if soup.title else 'pagina_sin_titulo'
+    
+    # Modificar el nombre eliminando caracteres no válidos y letras no inglesas
+    valid_name = re.sub(r'[^a-zA-Z0-9]', '_', page_title)
+    valid_name += '_codigo'
+    
+    # Crear carpeta para las imágenes
+    if not os.path.exists(valid_name):
+        os.makedirs(valid_name)
+    
+    # Listar y descargar los links de imágenes
+    img_tags = soup.find_all('img')
+    img_urls = [img['src'] for img in img_tags if img['src'].endswith('.jpg') or img['src'].endswith('t.jpg')]
+    
+    for img_url in img_urls:
+        if img_url.endswith('t.jpg'):
+            img_url = img_url.replace('t.jpg', '.jpg')
+        
+        img_data = requests.get(img_url).content
+        img_name = os.path.join(valid_name, os.path.basename(img_url))
+        
+        with open(img_name, 'wb') as img_file:
+            img_file.write(img_data)
+    
+    # Comprimir la carpeta en .CBZ
+    cbz_filename = f"{valid_name}.cbz"
+    with zipfile.ZipFile(cbz_filename, 'w') as cbz_file:
+        for root, _, files in os.walk(valid_name):
+            for file in files:
+                cbz_file.write(os.path.join(root, file), arcname=file)
+    
+    # Enviar el archivo al usuario
+    with open(cbz_filename, 'rb') as cbz_file:
+        bot.send_document(message.chat.id, cbz_file)
+    
+    # Limpiar archivos temporales
+    os.remove(cbz_filename)
+    for root, _, files in os.walk(valid_name):
+        for file in files:
+            os.remove(os.path.join(root, file))
+    os.rmdir(valid_name)
+
 
 if __name__ == '__main__':
     bot.polling()
+    
