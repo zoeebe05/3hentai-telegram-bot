bf38ddad07adbd949bfa6921e38481d48d74608f
zoeebe05 <zoeebe05@gmail.com>
2024-08-21 13:46:22 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,8 @@ import requests
 from bs4 import BeautifulSoup
 from telebot import TeleBot
 import zipfile
+from PIL import Image
+from io import BytesIO
 
 # Función para limpiar el nombre de la página
 def clean_filename(name):
@@ -62,61 +64,65 @@ def send_info(message):
 
     os.remove(image_path)
 
-@bot.message_handler(commands=['d'])
-def handle_d(message):
-    # Obtener el código enviado después del comando /d
-    codigo = message.text.split()[1]
-    url = f'https://es.3hentai.net/d/{codigo}'
+@bot.message_handler(commands=['ima'])
+def handle_ima_command(message):
+    code = message.text.split()[1]  # Suponiendo que el código es el segundo argumento
+    url = f"https://es.3hentai.org/d/{code}"
     
     response = requests.get(url)
-    if response.status_code != 200:
-        bot.reply_to(message, "No se pudo acceder a la página.")
-        return
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        image_links = [img['src'].replace('t.jpg', '.jpg') for img in soup.find_all('img') if img['src'].endswith('t.jpg')]
+        
+        for link in image_links:
+            image_response = requests.get(link)
+            if image_response.status_code == 200:
+                image = BytesIO(image_response.content)
+                bot.send_photo(message.chat.id, image)
+            else:
+                bot.reply_to(message, f"No se pudo descargar la imagen: {link}")
+    else:
+        bot.reply_to(message, "No se pudo acceder al enlace proporcionado.")
 
-    soup = BeautifulSoup(response.content, 'html.parser')
+
+@bot.message_handler(commands=['cbz'])
+def handle_cbz_command(message):
+    code = message.text.split()[1]  # Suponiendo que el código es el segundo argumento
+    url = f"https://es.3hentai.org/d/{code}"
     
-    # Capturar el nombre de la página
-    page_title = soup.title.string if soup.title else 'pagina_sin_titulo'
-    
-    # Modificar el nombre eliminando caracteres no válidos y letras no inglesas
-    valid_name = re.sub(r'[^a-zA-Z0-9]', '_', page_title)
-    valid_name += f'_{codigo}'
-    
-    # Crear carpeta para las imágenes
-    if not os.path.exists(valid_name):
-        os.makedirs(valid_name)
-    
-    # Listar y descargar los links de imágenes
-    img_tags = soup.find_all('img')
-    img_urls = [img['src'] for img in img_tags if img['src'].endswith('.jpg') or img['src'].endswith('t.jpg')]
-    
-    for img_url in img_urls:
-        if img_url.endswith('t.jpg'):
-            img_url = img_url.replace('t.jpg', '.jpg')
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        image_links = [img['src'].replace('t.jpg', '.jpg') for img in soup.find_all('img') if img['src'].endswith('t.jpg')]
         
-        img_data = requests.get(img_url).content
-        img_name = os.path.join(valid_name, os.path.basename(img_url))
+        images = []
+        for link in image_links:
+            image_response = requests.get(link)
+            if image_response.status_code == 200:
+                image = Image.open(BytesIO(image_response.content))
+                images.append(image)
+            else:
+                bot.reply_to(message, f"No se pudo descargar la imagen: {link}")
         
-        with open(img_name, 'wb') as img_file:
-            img_file.write(img_data)
-    
-    # Comprimir la carpeta en .CBZ
-    cbz_filename = f"{valid_name}.cbz"
-    with zipfile.ZipFile(cbz_filename, 'w') as cbz_file:
-        for root, _, files in os.walk(valid_name):
-            for file in files:
-                cbz_file.write(os.path.join(root, file), arcname=file)
-    
-    # Enviar el archivo al usuario
-    with open(cbz_filename, 'rb') as cbz_file:
-        bot.send_document(message.chat.id, cbz_file)
-    
-    # Limpiar archivos temporales
-    os.remove(cbz_filename)
-    for root, _, files in os.walk(valid_name):
-        for file in files:
-            os.remove(os.path.join(root, file))
-    os.rmdir(valid_name)
+        if images:
+            cbz_file = create_cbz(images, code)
+            with open(cbz_file, 'rb') as file:
+                bot.send_document(message.chat.id, file)
+            os.remove(cbz_file)
+        else:
+            bot.reply_to(message, "No se encontraron imágenes para crear el archivo CBZ.")
+    else:
+        bot.reply_to(message, "No se pudo acceder al enlace proporcionado.")
+
+def create_cbz(images, code):
+    cbz_filename = f"{code}.cbz"
+    with zipfile.ZipFile(cbz_filename, 'w') as cbz:
+        for i, image in enumerate(images):
+            image_filename = f"image_{i}.jpg"
+            image.save(image_filename)
+            cbz.write(image_filename)
+            os.remove(image_filename)
+    return cbz_filename
 
 if __name__ == '__main__':
     bot.polling()
